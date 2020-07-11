33225aa5057ea4e85db519e99239a322a8e18a23
Leedabord <gwforumlakers@gmail.com>
2020-07-10 18:57:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ion3fire1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ion3fire1)
---
+++ enviroments/enviroment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyAvB4R09YDHtfxOW7C3OQNLUGi9ce4qrws",
+  authDomain: "todo-firebase-17b28.firebaseapp.com",
+  databaseURL: "https://todo-firebase-17b28.firebaseio.com",
+  projectId: "todo-firebase-17b28"
+  }
+};
+
+/*
+    storageBucket: 'traveling-together.appspot.com',
+    messagingSenderId: '504041587250'
+  firebase: {
+  apiKey: "AIzaSyAvB4R09YDHtfxOW7C3OQNLUGi9ce4qrws",
+  authDomain: "todo-firebase-17b28.firebaseapp.com",
+  databaseURL: "https://todo-firebase-17b28.firebaseio.com",
+  projectId: "todo-firebase-17b28"
+*/
